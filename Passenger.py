fc4ce875e6b7f5979beadfb1d85a1f3caf3495a2
David Taylor <david@cloudartisan.com>
2011-05-20 21:45:23 +1000

    Added debugging

--- Passenger.py
+++ Passenger.py
@@ -10,9 +10,9 @@ STRIP_COLOUR='sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g"'
 
 # NB: These commands require the ability to run passenger-memory-stats and
 # passenger-status.  You might need to prepend the commands with variations
-# on 'sudo -u username -i' or some other concoction.
-PASSENGER_MEMORY_STATS_CMD = 'rvmsudo passenger-memory-stats | ' + STRIP_COLOUR
-PASSENGER_STATUS_CMD = 'rvmsudo passenger-status | ' + STRIP_COLOUR
+# on 'rvmsudo' or 'sudo -u username -i' or some other concoction.
+PASSENGER_MEMORY_STATS_CMD = 'passenger-memory-stats | ' + STRIP_COLOUR
+PASSENGER_STATUS_CMD = 'passenger-status | ' + STRIP_COLOUR
 
 
 class Passenger:
@@ -40,32 +40,43 @@ class Passenger:
         }
         status, out = commands.getstatusoutput(PASSENGER_STATUS_CMD)
         if status != 0:
+            self.checks_logger.error("Failed: %s" % PASSENGER_STATUS_CMD)
             return stats
 
         # max application instances
         match = re.search('max += (\d+)', out)
         if match:
             stats['max_application_instances'] = int(match.group(1))
+            self.checks_logger.debug('max_application_instances = %d' %
+                    stats['max_application_instances'])
 
         # count application instances
         match = re.search('count += (\d+)', out)
         if match:
             stats['count_application_instances'] = int(match.group(1))
+            self.checks_logger.debug('count_application_instances = %d' %
+                    stats['count_application_instances'])
 
         # active application instances
         match = re.search('active += (\d+)', out)
         if match:
             stats['active_application_instances'] = int(match.group(1))
+            self.checks_logger.debug('active_application_instances = %d' %
+                    stats['active_application_instances'])
 
         # inactive application instances
         match = re.search('inactive += (\d+)', out)
         if match:
             stats['inactive_application_instances'] = int(match.group(1))
+            self.checks_logger.debug('inactive_application_instances = %d' %
+                    stats['inactive_application_instances'])
 
         # waiting on global queue
         match = re.search('Waiting on global queue: (\d+)', out)
         if match:
             stats['waiting_on_global_queue'] = int(match.group(1))
+            self.checks_logger.debug('waiting_on_global_queue = %d' %
+                    stats['waiting_on_global_queue'])
 
         return stats
 
@@ -88,27 +99,36 @@ class Passenger:
         }
         status, out = commands.getstatusoutput(PASSENGER_MEMORY_STATS_CMD)
         if status != 0:
+            self.checks_logger.error("Failed: %s" % PASSENGER_MEMORY_STATS_CMD)
             return stats
 
         # Passenger watchdog memory
         match = re.search('\d+ +\d+\.?\d+ MB +(\d+\.?\d+) MB + PassengerWatchdog', out)
         if match:
             stats['passenger_watchdog_rss_mb'] = float(match.group(1))
+            self.checks_logger.debug('passenger_watchdog_rss_mb = %d' %
+                    stats['passenger_watchdog_rss_mb'])
 
         # Passenger helper agent memory
         match = re.search('\d+ +\d+\.?\d+ MB +(\d+\.?\d+) MB + PassengerHelperAgent', out)
         if match:
             stats['passenger_helper_agent_rss_mb'] = float(match.group(1))
+            self.checks_logger.debug('passenger_helper_agent_rss_mb = %d' %
+                    stats['passenger_helper_agent_rss_mb'])
 
         # Passenger spawn server memory
         match = re.search('\d+ +\d+\.?\d+ MB +(\d+\.?\d+) MB + Passenger spawn server', out)
         if match:
             stats['passenger_spawn_server_rss_mb'] = float(match.group(1))
+            self.checks_logger.debug('passenger_spawn_server_rss_mb = %d' %
+                    stats['passenger_spawn_server_rss_mb'])
 
         # Passenger logging agent memory
         match = re.search('\d+ +\d+\.?\d+ MB +(\d+\.?\d+) MB + PassengerLoggingAgent', out)
         if match:
             stats['passenger_logging_agent_rss_mb'] = float(match.group(1))
+            self.checks_logger.debug('passenger_logging_agent_rss_mb = %d' %
+                    stats['passenger_logging_agent_rss_mb'])
 
         # There are multiple sections, each with lines that match
         # the regex for totals, so we scan down to the section we're
@@ -125,11 +145,14 @@ class Passenger:
             processes_match = re.match('### Processes: (\d+)', line)
             if processes_match:
                 stats['processes'] = int(processes_match.group(1))
+                self.checks_logger.debug('processes = %d' % stats['processes'])
             # Total RSS used by passenger and rails processes.  Eg,
             # ### Total private dirty RSS: 2266.23 MB
             total_private_dirty_rss_mb_match = re.match('### Total private dirty RSS: (\d+\.?\d+) MB', line)
             if total_private_dirty_rss_mb_match:
                 stats['total_private_dirty_rss_mb'] = float(total_private_dirty_rss_mb_match.group(1))
+                self.checks_logger.debug('total_private_dirty_rss_mb = %d' %
+                        stats['total_private_dirty_rss_mb'])
 
         return stats
 
@@ -141,5 +164,9 @@ class Passenger:
 
 
 if __name__ == "__main__":
-    passenger = Passenger(None, None, None)
-    print passenger.run()
+    import logging
+    logger = logging.getLogger("Passenger")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    passenger = Passenger(None, logger, None)
+    passenger.run()
